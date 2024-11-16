f32ca7c99dd2df5d5ab4aefcb3e8a263f6e23be6
kimminki10 <pangshe10@gmail.com>
2024-11-16 19:10:50 +0900

    change secret key add env file

--- trendboard/settings.py
+++ trendboard/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-q$3l&)r13$f+vufq!$_49(k*nsxit1g_4uyl(pms0ycrbcofe8'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
